d20676229eefa2eaf713c9ce7cee988b7fee5ef2
EoH <iagogoliver@gmail.com>
2020-11-30 11:09:27 -0300

    teste

---
+++ PastaPy/gitpush.py
@@ -0,0 +1,11 @@
+import os
+
+nomeArquivo = str(input('Insira o nome do arquivo/pasta desejado: '))
+commit = str(input('Insira o comentario do commit: '))
+path = 'cd projetos/projetos'
+
+os.system(path)
+
+os.system('git add '+ nomeArquivo)
+os.system('git commit - m ' + commit)
+os.system('git push')
