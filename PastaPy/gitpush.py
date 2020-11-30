340cfdaee0bb178b2968d9cabba99903822826e3
EoH <iagogoliver@gmail.com>
2020-11-30 11:48:17 -0300

    Script para upar arquivos no github

--- PastaPy/gitpush.py
+++ PastaPy/gitpush.py
@@ -1,6 +1,6 @@
 import os
 while True:
-    inputUsuario = int(input('Qual ação é desejada? 1 - Git Status  2 - Git add/commit/push'))
+    inputUsuario = int(input('Qual ação é desejada? 1 - Git Status  2 - Git add/commit/push '))
     if inputUsuario == 1:
       os.system("cd C:\\Users\\exile\\projetos\\projetos")
       print(os.system("git status"))
