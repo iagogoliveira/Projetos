5ae2988f29db9af3894956162867dcbeb8ce469d
EoH <iagogoliver@gmail.com>
2020-11-30 12:24:31 -0300

    att

--- gitpush.py
+++ gitpush.py
@@ -2,18 +2,24 @@ import os
 while True:
     print('//////////////////////////////////////////////////////')
     print(' ')
-    inputUsuario = int(input('Qual ação é desejada? 1 - Git Status  2 - Git add/commit/push '))
+    inputUsuario = int(input('Qual ação é desejada? 1 - Git Status  2 - Git add/commit/push 3 - Visualizar arquivos no repositorio  4 - Git rm' ))
     if inputUsuario == 1:
-      os.system("cd C:\\Users\\exile\\projetos\\projetos")
       print(os.system("git status"))
     elif inputUsuario == 2:  
-        os.system("cd C:\\Users\\exile\\projetos\\projetos")
-        nomeArquivo = str(input('Insira o nome do arquivo/pasta desejado: '))
-        commit = str(input('Insira o comentario do commit: '))
+          nomeArquivo = str(input('Insira o nome do arquivo/pasta desejado: '))
+          commit = str(input('Insira o comentario do commit: '))
 
-        os.system('git add '+ nomeArquivo)
-        os.system('git commit -m '+ commit )
-        os.system('git push')
+          os.system('git add '+ nomeArquivo)
+          os.system('git commit -m '+ commit )
+          os.system('git push')
+    elif inputUsuario == 3:
+          os.system('dir')
+    elif inputUsuario == 4:
+          nomeArquivoRemov = str(input('Insira o nome do arquivo/pasta desejado para remoção: '))
+          commit = str(input('Insira o comentario do commit: '))
+          os.system('git rm -r ' + nomeArquivoRemov)
+          os.system('git commit -m '+ commit )
+          os.system('git push')
     else:
         print('Ação inválida!')    
 
